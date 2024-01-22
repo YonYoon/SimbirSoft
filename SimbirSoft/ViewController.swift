b6847c2aea6dddf6650b71c08058bd49933afc89
YonYoon <zhzhzh1806@gmail.com>
2024-01-22 11:03:04 +0500

    Initial Commit

---
+++ SimbirSoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimbirSoft
+//
+//  Created by Zhansen Zhalel on 22.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
